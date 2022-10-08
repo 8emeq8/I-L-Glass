571c55f3c9c2aba6628b0cc5aa023c19f17836b8
8emeq8 <85879063+8emeq8@users.noreply.github.com>
2022-10-08 13:10:49 +0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    'name': 'Sales Extended',
+    'version': '15.0.1.0.0',
+    'summary': '',
+    'description': """""",
+    'author': '',
+    'company': '',
+    'maintainer': '',
+    'category': '',
+    'website': '',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_view.xml',
+        'report/sale_report.xml',
+    ],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
